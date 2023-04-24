8c42f3c3e12de4e13acc20eb7b08dc90fefde5ca
lel020 <mahmud.jawaad03@gmail.com>
2023-04-25 09:53:54 +1000

    Renamed variable

--- src/script.js
+++ src/script.js
@@ -1,8 +1,9 @@
 const addButton = document.getElementById('add');
-const toDoContainer = document.getElementById('todoTextList');
+const toDoTxt = document.getElementById('todoTextList');
 const inputField = document.getElementById('input');
 
 addButton.addEventListener('click', () => {
     const paragraph = document.createElement('p');
-    toDoContainer.appendChild(paragraph);
+    paragraph.innerText = inputField.value;
+    toDoTxt.appendChild(paragraph);
 });
