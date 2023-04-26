d7b3361966f4fefd0731b18eacff9cc95e5cea16
lel020 <mahmud.jawaad03@gmail.com>
2023-04-26 10:25:04 +1000

    Added char length limit, so doesn't go off screen

--- src/script.js
+++ src/script.js
@@ -2,21 +2,30 @@ const addButton = document.getElementById('add');
 const clearButton = document.getElementById('clear');
 const toDoTxt = document.getElementById('todoTextList');
 const inputField = document.getElementById('input');
-//max number of items the list can store
+
 let numOfItems = 0;
 const maxNumOfItems = 20;
+const maxCharacters = 42;
 
 addButton.addEventListener('click', () => {
+    const inputValue = inputField.value;
+    const maxInputChar = inputValue.length;
+
     if(inputField.value !== ''){
         if(numOfItems < maxNumOfItems){
-            const newTodo = document.createElement('li');
-            newTodo.textContent = inputField.value;
-            toDoTxt.appendChild(newTodo);
-            inputField.value = '';
-            numOfItems++;
+            if(maxInputChar <= maxCharacters){
+                const newTodo = document.createElement('li');
+                newTodo.textContent = inputField.value;
+                toDoTxt.appendChild(newTodo);
+                inputField.value = '';
+                numOfItems++;
+            }
+            else{
+                alert("Max character limit: 42 with spaces (inclusive)");
+            }
         }
         else{
-            alert("Max number of items to be listed has been reached!");
+            alert("Max number of items: 20");
         }
     }
 });
