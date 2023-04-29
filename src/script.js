8abb9a1da53a47e6278f2f3ba92eb482cf811280
lel020 <mahmud.jawaad03@gmail.com>
2023-04-29 14:49:09 +1000

    Todo list data is now saved to local storage

--- src/script.js
+++ src/script.js
@@ -7,6 +7,12 @@ let numOfItems = 0;
 const maxNumOfItems = 20;
 const maxCharacters = 100;
 
+// Check if there are any items in local storage
+if(localStorage.getItem('todos')){
+  toDoTxt.innerHTML = localStorage.getItem('todos');
+  numOfItems = toDoTxt.children.length;
+}
+
 //When user clicks on the 'add' button
 addButton.addEventListener('click', () => {
     const inputValue = inputField.value;
@@ -20,6 +26,9 @@ addButton.addEventListener('click', () => {
                 toDoTxt.appendChild(newTodo);
                 inputField.value = '';
                 numOfItems++;
+
+                // Save the updated list to local storage
+                localStorage.setItem('todos', toDoTxt.innerHTML);
             }
             else{
                 alert("Max character limit: 42 with spaces (inclusive)");
@@ -45,6 +54,9 @@ clearButton.addEventListener('click', () => {
         inputField.value = '';
         toDoTxt.textContent = '';
         numOfItems = 0;
+
+        // Remove the list from local storage
+        localStorage.removeItem('todos');
     }
     else{
         console.error("An ERROR has occurred");
